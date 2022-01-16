cfdb23767183310d791148c65596945dd9470746
SELLOUTI, Hamza <hasellou@bouyguestelecom.fr>
2022-01-16 15:28:52 +0100

    ✨ initial home screen

---
+++ src/Requests.js
@@ -0,0 +1,14 @@
+const API_KEY = "ba7b1aee09b03b99d53e457dab3221b4";
+
+const requests = {
+  fetchTrend: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_network=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genre=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genre=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genre=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genre=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genre=99`,
+};
+
+export default requests;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAapMAxc3Ixv7-h7vCGDPJxj6nfMlTmZMQ",
+    authDomain: "netflix-clone-c59d7.firebaseapp.com",
+    projectId: "netflix-clone-c59d7",
+    storageBucket: "netflix-clone-c59d7.appspot.com",
+    messagingSenderId: "922123736497",
+    appId: "1:922123736497:web:aee207aa717424328e9990",
+    measurementId: "G-K84MRVJD9T"
+  };
